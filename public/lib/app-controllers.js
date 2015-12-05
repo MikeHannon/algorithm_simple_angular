8610896f3063eeb8615ba0747f9cbe4917351f29
Mike Hannon(code) <mjhannon@gmail.com>
2015-12-05 13:27:01 -0800

    add codemirror

--- public/lib/app-controllers.js
+++ public/lib/app-controllers.js
@@ -4,33 +4,58 @@ app.controller('MasterController', ['$scope', function ($scope) {
 app.controller('algorithmController', ['$scope','algorithmFactory','algorithmFactoryJSON', function ($scope, algorithmFactory, algorithmFactoryJSON) {
   algorithmFactory.set_path('algorithms');
   algorithmFactoryJSON.set_path('algorithms');
-  // $scope.y = {name: "mike"};
+  $scope.show_new = 'False';
+  $scope.show_show = 'False';
   $scope.data_set = algorithmFactory.return_data;
   $scope.data_setJSON = algorithmFactoryJSON.return_dataJSON;
-  var edit_dataSet = function(data){
-    console.log(data);
-    // for (element in data){
-    //   console.log(data[element]);
-    //   var new_element = data[element].split("↵").join('');
-    //   console.log(new_element);
-    // }
+
+  $scope.index_button = function()
+  {
+    algorithmFactory.index();
+    $scope.show_show = 'False';
   }
-  $scope.index_button = function(){algorithmFactory.index();}
-  $scope.create_button = function(algorithm){
+
+  $scope.create_button = function(algorithm)
+  {
     algorithmFactory.create(algorithm);
-    
+    $scope.show_new = 'False';
+    $scope.show_show = 'False';
+    algorithm = {};
+    $scope.index_buttonJSON();
   }
-  $scope.new_button = function(){algorithmFactory.new();}
-  $scope.update_button = function(algorithm){algorithmFactory.update(algorithm);}
+
+  $scope.new_button = function()
+  {
+    $scope.show_new = 'True';
+    $scope.show_show = 'False';
+    algorithmFactory.new();
+  }
+
+  $scope.update_button = function(algorithm)
+  {
+    algorithmFactory.update(algorithm);
+  }
+
   $scope.edit_button = function(id){algorithmFactory.edit(id);}
   $scope.delete_button = function(id){algorithmFactory.delete(id);}
   $scope.show_button = function(id){
     algorithmFactoryJSON.show(id);
     algorithmFactory.show(id);
+    $scope.show_show = 'True';
+    $scope.show_new = 'False';
     }
 
-  $scope.index_buttonJSON = function(){algorithmFactoryJSON.index();}
-  $scope.create_buttonJSON = function(algorithm){algorithmFactoryJSON.create(algorithm);}
+  $scope.index_buttonJSON = function()
+  {
+    algorithmFactoryJSON.index();
+    $scope.show_show = 'False';
+    $scope.show_new = 'False';
+  }
+  $scope.create_buttonJSON = function(algorithm){
+    algorithmFactoryJSON.create(algorithm);
+    $scope.show_show = 'False';
+    $scope.show_new = 'False';
+  }
   $scope.new_buttonJSON = function(){algorithmFactoryJSON.new();}
   $scope.update_buttonJSON = function(algorithm){algorithmFactoryJSON.update(algorithm);}
   $scope.edit_buttonJSON = function(id){algorithmFactoryJSON.edit(id);}
